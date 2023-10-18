661e08f128c6b688c1f9dc6187bb84b63034e47f
Victor Matheus <victormath23@outlook.com>
2023-10-17 21:40:18 -0300

    navegação do site conluida

---
+++ PI-SENAC/projetoPI/projetoPI/src/main/java/com/senac/api/filmes_api/controller/WebController.java
@@ -0,0 +1,24 @@
+package com.senac.api.filmes_api.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/index")  // Mapeamento para a página principal
+    public String abrirPaginaPrincipal() {
+        return "index";  // Nome do arquivo HTML da página principal
+    }
+
+    @GetMapping("/consulta")  // Mapeamento para a página consulta.html
+    public String abrirPaginaConsulta() {
+        return "consulta";  // Nome do arquivo HTML da página consulta
+    }
+
+    @GetMapping("/login")  // Mapeamento para a página login.html
+    public String abrirPaginaLogin() {
+        return "login";  // Nome do arquivo HTML da página login
+    }
+}
